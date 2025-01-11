2f36ec80fe04edf505e69fcc13e68f0d7c8b4060
Aseubel <1228392319@qq.com>
2025-01-11 19:57:46 +0800

    feat: jwt属性配置

---
+++ utopia-app/src/main/java/com/aseubel/properties/JwtProperties.java
@@ -0,0 +1,17 @@
+package com.aseubel.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt.config")
+@Data
+public class JwtProperties {
+
+    private String secretKey;
+
+    private Long ttl = 604800017L;    // 过期时间，默认一星期
+
+    private String tokenName;
+}
---
+++ utopia-types/src/main/java/com/aseubel/types/common/Constant.java
@@ -0,0 +1,8 @@
+package com.aseubel.types.common;
+
+public class Constant {
+
+    // 过期时间，单位毫秒
+    public static final Long EXPIRE_TIME = 30 * 24 * 3600 * 1000L; // 一个月
+
+}
