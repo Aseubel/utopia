b8765da2391cb764f1bad08db3074704396f50d8
Aseubel <1228392319@qq.com>
2025-01-12 18:19:46 +0800

    feat: 微信登录api常量定义

--- utopia-types/src/main/java/com/aseubel/types/common/Constant.java
+++ utopia-types/src/main/java/com/aseubel/types/common/Constant.java
@@ -5,4 +5,6 @@ public class Constant {
     // 过期时间，单位毫秒
     public static final Long EXPIRE_TIME = 30 * 24 * 3600 * 1000L; // 一个月
 
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
 }
