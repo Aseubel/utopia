a7e01a93599932d12ea48edddab0b18ca424300b
Aseubel <1228392319@qq.com>
2025-01-13 12:15:39 +0800

    feat: 查询用户信息Controller

--- utopia-trigger/src/main/java/com/aseubel/trigger/http/UserController.java
+++ utopia-trigger/src/main/java/com/aseubel/trigger/http/UserController.java
@@ -3,9 +3,12 @@ package com.aseubel.trigger.http;
 import com.aseubel.api.UserInterface;
 import com.aseubel.api.dto.user.LoginRequestDTO;
 import com.aseubel.api.dto.user.LoginResponseDTO;
+import com.aseubel.api.dto.user.QueryUserInfoRequestDTO;
+import com.aseubel.api.dto.user.QueryUserInfoResponseDTO;
 import com.aseubel.domain.user.model.UserEntity;
 import com.aseubel.domain.user.service.IUserService;
 import com.aseubel.types.Response;
+import com.aseubel.types.constraint.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -19,7 +22,7 @@ import javax.validation.Valid;
 @RestController()
 @Validated
 @CrossOrigin("${app.config.cross-origin}")
-@RequestMapping("/api/${app.config.api-version}/user/")
+@RequestMapping("/api/v1/user/") //${app.config.api-version}
 @RequiredArgsConstructor
 public class UserController implements UserInterface {
 
@@ -32,6 +35,24 @@ public class UserController implements UserInterface {
 
         return Response.SYSTEM_SUCCESS(
                 LoginResponseDTO.builder()
+                        .userid(user.getOpenid())
+                        .userName(user.getUserName())
+                        .realName(user.getRealName())
+                        .phone(user.getPhone())
+                        .gender(user.getGender())
+                        .avatar(user.getAvatar())
+                        .signature(user.getSignature())
+                        .token(user.getToken())
+                        .build());
+    }
+
+    @Override
+    @GetMapping("/info")
+    public Response<QueryUserInfoResponseDTO> queryUserInfo(@Valid @RequestBody QueryUserInfoRequestDTO queryUserInfoRequestDTO) {
+        UserEntity user = userService.queryUserInfo(queryUserInfoRequestDTO.getUserId());
+
+        return Response.SYSTEM_SUCCESS(
+                QueryUserInfoResponseDTO.builder()
                         .userid(user.getOpenid())
                         .userName(user.getUserName())
                         .realName(user.getRealName())
