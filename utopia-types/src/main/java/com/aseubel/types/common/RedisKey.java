e107632a8da0197af952514ff72a2663cb5dc8de
Aseubel <1228392319@qq.com>
2025-01-15 09:12:43 +0800

    feat: 1. 添加redis的key常量

---
+++ utopia-types/src/main/java/com/aseubel/types/common/RedisKey.java
@@ -0,0 +1,18 @@
+package com.aseubel.types.common;
+
+/**
+ * @author Aseubel
+ * @description Redis key
+ * @date 2025-01-15 08:25
+ */
+public class RedisKey {
+
+    public static final String SPLIT = ":";
+
+    public static final String PREFIX = "utopia";
+
+    public static final String USER_TOKEN = "user:token";
+
+    public static final String USER_INFO = "user:info";
+
+}
