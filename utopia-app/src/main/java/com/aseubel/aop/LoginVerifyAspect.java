d8f71a7513d709af3004d7dc766b6b4ab81be8c3
Aseubel <1228392319@qq.com>
2025-01-15 10:35:56 +0800

    feat: 1. 双token后的aop登录校验

--- utopia-app/src/main/java/com/aseubel/aop/LoginVerifyAspect.java
+++ utopia-app/src/main/java/com/aseubel/aop/LoginVerifyAspect.java
@@ -1,10 +1,13 @@
 package com.aseubel.aop;
 
+import com.aseubel.domain.user.adapter.repo.IUserRepository;
+import com.aseubel.infrastructure.redis.IRedisService;
 import com.aseubel.types.util.JwtUtil;
 import com.aseubel.properties.JwtProperties;
 import com.aseubel.types.Response;
 import com.aseubel.types.enums.GlobalServiceStatusCode;
 import com.aseubel.types.exception.AppException;
+import com.aseubel.types.util.RedisKeyBuilder;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -17,9 +20,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Optional;
+
+import static com.aseubel.types.common.Constant.ACCESS_TOKEN;
+import static com.aseubel.types.common.Constant.USER_ID_KEY;
 
 /**
  * @author Aseubel
@@ -33,6 +41,12 @@ public class LoginVerifyAspect {
 
     @Autowired
     private JwtProperties jwtProperties;
+
+    @Resource
+    private IUserRepository userRepository;
+
+    @Resource
+    private IRedisService redisService;
 
     /**
      * 拦截入口
@@ -53,19 +67,49 @@ public class LoginVerifyAspect {
         String userId = (String) arg.getClass().getMethod("getUserId").invoke(arg);
         String token = request.getHeader(jwtProperties.getTokenName());
 
-        // 校验令牌
         try {
-            Claims claims = JwtUtil.parseJWT(jwtProperties.getSecretKey(), token);
-            log.info("用户进行jwt校验通过，id:{}，token:{}", userId, token);
-            return point.proceed();
-        } catch (AppException e) {
-            // 不通过，响应401状态码
-            log.error("用户进行jwt校验失败！id:{}，token:{}", userId, token);;
-            if (response != null) {
-                response.setStatus(401);
+            // 校验redis中是否有token，没有就是过期
+            log.info("redis校验token，id:{}，token:{}", userId, token);
+            String access_token = redisService.getFromMap(
+                    RedisKeyBuilder.UserTokenKey(userId), ACCESS_TOKEN);
+            // token为空过期
+            if (access_token == null) {
+                Optional.ofNullable(response).ifPresent(r -> r.setStatus(401));
+                return Response.CUSTOMIZE_ERROR(GlobalServiceStatusCode.USER_TOKEN_EXPIRED);
             }
-            return Response.CUSTOMIZE_ERROR(GlobalServiceStatusCode.USER_TOKEN_ERROR);
+            // 校验token是否正确
+            if (!access_token.equals(token)) {
+                Optional.ofNullable(response).ifPresent(r -> r.setStatus(401));
+                return Response.CUSTOMIZE_ERROR(GlobalServiceStatusCode.USER_TOKEN_ERROR);
+            }
+        } catch (Exception e) {
+            log.error("redis校验token失败！id:{}，token:{}", userId, token);
+            // redis宕机，解码校验令牌
+            try {
+                Claims claims = JwtUtil.parseJWT(jwtProperties.getSecretKey(), token);
+                // 校验用户id
+                if (!claims.get(USER_ID_KEY).equals(userId)) {
+                    throw new AppException("用户id与token不匹配！");
+                }
+                log.info("用户进行jwt校验通过，id:{}，token:{}", userId, token);
+                return point.proceed();
+            } catch (Exception ex) {
+                // 不通过，响应401状态码
+                log.error("用户进行jwt校验失败！id:{}，token:{}", userId, token);;
+                if (response != null) {
+                    response.setStatus(401);
+                }
+                return Response.CUSTOMIZE_ERROR(GlobalServiceStatusCode.USER_TOKEN_ERROR);
+            }
         }
+        String newToken = userRepository.generateUserToken(
+                userId, jwtProperties.getSecretKey(), jwtProperties.getAccess_ttl());
+        // 更新redis中token
+        redisService.addToMap(RedisKeyBuilder.UserTokenKey(userId), ACCESS_TOKEN, newToken);
+        Optional.ofNullable(response)
+                .ifPresent(r -> r.setHeader(jwtProperties.getTokenName(), newToken));
+        log.info("用户进行登录校验通过，id:{}，token:{}", userId, newToken);
+        return point.proceed();
     }
 
 }
