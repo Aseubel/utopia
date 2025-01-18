dcf5c8cdbd68677023dadfb927bab4310bbc9f62
Aseubel <1228392319@qq.com>
2025-01-18 11:22:32 +0800

    feat: 构建rediskey加注释

--- utopia-types/src/main/java/com/aseubel/types/util/RedisKeyBuilder.java
+++ utopia-types/src/main/java/com/aseubel/types/util/RedisKeyBuilder.java
@@ -9,10 +9,22 @@ import static com.aseubel.types.common.RedisKey.*;
  */
 public class RedisKeyBuilder {
 
+    /**
+     * 构造用户token的redis key
+     * utopia:user:token:{userId}
+     * @param userId 用户id
+     * @return redis key
+     */
     public static String UserTokenKey(String userId) {
         return PREFIX + SPLIT + USER_TOKEN + SPLIT + userId;
     }
 
+    /**
+     * 构造用户信息的redis key
+     * utopia:user:info:{userId}
+     * @param userId 用户id
+     * @return redis key
+     */
     public static String UserInfoKey(String userId) {
         return PREFIX + SPLIT + USER_INFO + SPLIT + userId;
     }
