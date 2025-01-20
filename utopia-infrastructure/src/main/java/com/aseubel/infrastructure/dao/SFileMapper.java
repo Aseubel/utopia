cdbaab124a11b7c27e68f545feb5916b48f753e2
Aseubel <1228392319@qq.com>
2025-01-20 16:54:15 +0800

    feat: 保存分享文件信息dao

---
+++ utopia-infrastructure/src/main/java/com/aseubel/infrastructure/dao/SFileMapper.java
@@ -0,0 +1,15 @@
+package com.aseubel.infrastructure.dao;
+
+import com.aseubel.infrastructure.dao.po.SFile;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SFileMapper {
+
+    /**
+     * 添加文件
+     * @param file
+     */
+    void addSFile(SFile file);
+
+}
