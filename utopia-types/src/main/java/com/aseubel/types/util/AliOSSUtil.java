b68e5e2fe50b39150fca71ba8650c0fdbb87f25c
Aseubel <1228392319@qq.com>
2025-01-20 15:30:43 +0800

    feat: 错误日志打印格式

--- utopia-types/src/main/java/com/aseubel/types/util/AliOSSUtil.java
+++ utopia-types/src/main/java/com/aseubel/types/util/AliOSSUtil.java
@@ -153,7 +153,7 @@ public class AliOSSUtil {
 
             // 完成分片上传。
             CompleteMultipartUploadResult completeMultipartUploadResult = ossClient.completeMultipartUpload(completeMultipartUploadRequest);
-            System.out.println(completeMultipartUploadResult.getETag());
+            // System.out.println(completeMultipartUploadResult.getETag());
         } catch (OSSException oe) {
             log.error("Caught an OSSException, which means your request made it to OSS, "
                     + "but was rejected with an error response for some reason.");
@@ -190,7 +190,6 @@ public class AliOSSUtil {
     }
 
     public byte[] download(String objectName) throws com.aliyuncs.exceptions.ClientException {
-        objectName = APP + "/" + objectName;
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
         EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
 
@@ -267,17 +266,17 @@ public class AliOSSUtil {
             // 删除文件或目录。如果要删除目录，目录必须为空。
             ossClient.deleteObject(BUCKET_NAME, objectName);
         } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+            log.error("Caught an OSSException, which means your request made it to OSS, "
                     + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
+            log.error("Error Message:{}", oe.getErrorMessage());
+            log.error("Error Code:{}", oe.getErrorCode());
+            log.error("Request ID:{}", oe.getRequestId());
+            log.error("Host ID:{}", oe.getHostId());
         } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
+            log.error("Caught an ClientException, which means the client encountered "
                     + "a serious internal problem while trying to communicate with OSS, "
                     + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
+            log.error("Error Message:{}", ce.getMessage());
         } finally {
             if (ossClient != null) {
                 ossClient.shutdown();
