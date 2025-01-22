02f86e26eb4b40009e98f2e671c28b863826fe14
Aseubel <1228392319@qq.com>
2025-01-22 13:25:28 +0800

    feat: 讨论帖子实体类

---
+++ utopia-domain/src/main/java/com/aseubel/domain/community/model/entity/DiscussPostEntity.java
@@ -0,0 +1,58 @@
+package com.aseubel.domain.community.model.entity;
+
+import com.aseubel.types.annotation.FieldDesc;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author Aseubel
+ * @description 社区讨论帖子实体类
+ * @date 2025-01-22 12:21
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DiscussPostEntity {
+
+    @FieldDesc(name = "帖子id")
+    private String discussPostId;
+
+    @FieldDesc(name = "用户id")
+    private String userId;
+
+    @FieldDesc(name = "标题")
+    private String title;
+
+    @FieldDesc(name = "内容")
+    private String content;
+
+    @FieldDesc(name = "点赞数")
+    private Integer likeCount;
+
+    @FieldDesc(name = "评论数")
+    private Integer commentCount;
+
+    @FieldDesc(name = "转发数")
+    private Integer forwardCount;
+
+    @FieldDesc(name = "0-普通;1-置顶")
+    private Integer type;
+
+    @FieldDesc(name = "0-普通;1-封禁")
+    private Integer status;
+
+    @FieldDesc(name = "创建时间")
+    private String createTime;
+
+    @FieldDesc(name = "更新时间")
+    private String updateTime;
+
+    @FieldDesc(name = "图片list")
+    private List<String> images;
+
+}
