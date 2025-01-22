522afb28bb6f78193ff391e252af98bb22c526f3
Aseubel <1228392319@qq.com>
2025-01-22 13:34:52 +0800

    feat: 列出所有帖子mapper接口

--- utopia-infrastructure/src/main/java/com/aseubel/infrastructure/dao/DiscussPostMapper.java
+++ utopia-infrastructure/src/main/java/com/aseubel/infrastructure/dao/DiscussPostMapper.java
@@ -3,6 +3,8 @@ package com.aseubel.infrastructure.dao;
 import com.aseubel.infrastructure.dao.po.DiscussPost;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author Aseubel
  * @description 社区帖子数据库操作接口
@@ -35,5 +37,13 @@ public interface DiscussPostMapper {
      * @return
      */
     DiscussPost getDiscussPostByPostId(String postId);
+
+    /**
+     * 获取所有帖子信息，分页查询
+     * @param postId 上一页最后一条帖子的id，用于分页查询
+     * @param limit 每页显示的数量
+     * @return
+     */
+    List<DiscussPost> listDiscussPost(String postId, Integer limit);
 
 }
