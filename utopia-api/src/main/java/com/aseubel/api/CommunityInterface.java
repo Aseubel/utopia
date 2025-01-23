8d52847a8a843fbebe0a8ccd2df2dd278a4799ce
Aseubel <1228392319@qq.com>
2025-01-23 19:30:41 +0800

    feat: 获取主页帖子列表接口

---
+++ utopia-api/src/main/java/com/aseubel/api/CommunityInterface.java
@@ -0,0 +1,20 @@
+package com.aseubel.api;
+
+import com.aseubel.api.dto.community.QueryIndexDiscussPostRequestDTO;
+import com.aseubel.api.dto.community.QueryIndexDiscussPostResponseDTO;
+import com.aseubel.types.Response;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * @author Aseubel
+ * @description 社区请求接口
+ * @date 2025-01-23 19:23
+ */
+public interface CommunityInterface {
+
+    Response<List<QueryIndexDiscussPostResponseDTO>> queryIndexDiscussPost(@Valid @RequestBody QueryIndexDiscussPostRequestDTO requestDTO);
+
+}
---
+++ utopia-trigger/src/main/java/com/aseubel/trigger/http/CommunityController.java
@@ -0,0 +1,55 @@
+package com.aseubel.trigger.http;
+
+import com.aseubel.api.CommunityInterface;
+import com.aseubel.api.dto.community.QueryIndexDiscussPostRequestDTO;
+import com.aseubel.api.dto.community.QueryIndexDiscussPostResponseDTO;
+import com.aseubel.domain.community.model.entity.DiscussPostEntity;
+import com.aseubel.domain.community.service.ICommunityService;
+import com.aseubel.types.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController()
+@Validated
+@CrossOrigin("${app.config.cross-origin}")
+@RequestMapping("/api/v1/community/") //${app.config.api-version}
+@RequiredArgsConstructor
+public class CommunityController implements CommunityInterface {
+
+    private final ICommunityService communityService;
+
+    @Override
+    @GetMapping("/post")
+    public Response<List<QueryIndexDiscussPostResponseDTO>> queryIndexDiscussPost(QueryIndexDiscussPostRequestDTO requestDTO) {
+        List<DiscussPostEntity> discussPosts = communityService.listDiscussPost(requestDTO.getPostId(), requestDTO.getLimit());
+        List<QueryIndexDiscussPostResponseDTO> responseDTOs = new ArrayList<>();
+        for (DiscussPostEntity discussPost : discussPosts) {
+           responseDTOs.add(QueryIndexDiscussPostResponseDTO.builder()
+                   .discussPostId(discussPost.getDiscussPostId())
+                   .userId(discussPost.getUserId())
+                   .title(discussPost.getTitle())
+                   .content(discussPost.getContent())
+                   .likeCount(discussPost.getLikeCount())
+                   .commentCount(discussPost.getCommentCount())
+                   .forwardCount(discussPost.getForwardCount())
+                   .type(discussPost.getType())
+                   .status(discussPost.getStatus())
+                   .createTime(discussPost.getCreateTime())
+                   .updateTime(discussPost.getUpdateTime())
+                   .images(discussPost.getImages())
+                   .comments(discussPost.getComments())
+                   .build());
+        }
+        return Response.SYSTEM_SUCCESS(responseDTOs);
+    }
+
+}
