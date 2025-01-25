16f51a8cd55de041e887e3a3de143ff78b1fb49c
Aseubel <1228392319@qq.com>
2025-01-25 11:13:33 +0800

    feat: sql查询用户的头像昵称

--- utopia-infrastructure/src/main/java/com/aseubel/infrastructure/dao/UserMapper.java
+++ utopia-infrastructure/src/main/java/com/aseubel/infrastructure/dao/UserMapper.java
@@ -2,6 +2,9 @@ package com.aseubel.infrastructure.dao;
 
 import com.aseubel.infrastructure.dao.po.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
@@ -11,7 +14,7 @@ public interface UserMapper {
      * @param userId 用户id
      * @return 用户信息
      */
-    User getUserByUserId(String userId);
+    User getUserByUserId(@Param("userId") String userId);
 
     /**
      * 添加用户
@@ -24,5 +27,12 @@ public interface UserMapper {
      * @param user 用户信息
      */
     void updateUser(User user);
+
+    /**
+     * 在帖子、评论板块、点赞板块等地方显示用户的头像和昵称
+     * @param userIds
+     * @return
+     */
+    List<User> listUserAvatarAndNameByUserIds(List<String> userIds);
 
 }
