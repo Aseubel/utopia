efc86d8602378f3e2ac1b46b5042fd84dbcc2979
Aseubel <1228392319@qq.com>
2025-01-27 12:02:47 +0800

    feat: 上传帖子图片DTO

---
+++ utopia-api/src/main/java/com/aseubel/api/dto/community/UploadDiscussPostImageResponse.java
@@ -0,0 +1,26 @@
+package com.aseubel.api.dto.community;
+
+import com.aseubel.types.annotation.FieldDesc;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author Aseubel
+ * @description 上传帖子图片返回图片信息
+ * @date 2025-01-27 11:41
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadDiscussPostImageResponse implements Serializable {
+
+    @FieldDesc(name = "图片id")
+    private String imageId;
+
+    @FieldDesc(name = "图片url")
+    private String imageUrl;
+
+}
