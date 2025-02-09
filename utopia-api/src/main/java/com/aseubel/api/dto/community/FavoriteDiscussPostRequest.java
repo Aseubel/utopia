370282f9270567c0ceac29683d2bf81fca9d2d74
Aseubel <1228392319@qq.com>
2025-02-09 11:44:27 +0800

    feat: 定义收藏帖子请求dto

---
+++ utopia-api/src/main/java/com/aseubel/api/dto/community/FavoriteDiscussPostRequest.java
@@ -0,0 +1,26 @@
+package com.aseubel.api.dto.community;
+
+import com.aseubel.types.annotation.FieldDesc;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author Aseubel
+ * @description 收藏帖子请求参数
+ * @date 2025-02-08 19:39
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FavoriteDiscussPostRequest implements Serializable {
+
+    @FieldDesc(name = "用户id")
+    private String userId;
+
+    @FieldDesc(name = "帖子id")
+    private String postId;
+    
+}
